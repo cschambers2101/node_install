6760c897ac6a99860ec4a368ce6b015116c3236f
Craig Chambers <cchambers@salisbury6c.ac.uk>
2021-05-21 11:14:44 +0100

    Update to use nvm

--- install.sh
+++ install.sh
@@ -1,23 +1,6 @@
 #!/bin/bash!
 
-# Installs nodejs from the Nodesource PPA
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.35.2/install.sh | bash
 
-# add signing key
-curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt-get install -y nodejs
-
-# set the required version
-VERSION=node_16.x
-
-# get distro version
-DISTRO="$(lsb_release -s -c)"
-
-# add ppa to source list
-echo "deb https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-echo "deb-src https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee -a /etc/apt/sources.list.d/nodesource.list
-
-# install nodejs from ppa
-sudo apt-get update
-sudo apt-get install nodejs
-
+nvm install 16.1.0
 
