24c8b9bf8573666985205012c777a7a9e5ae53bd
Craig Chambers <cchambers@salisbury6c.ac.uk>
2021-05-21 11:17:18 +0100

    Updated instructions

--- install.sh
+++ install.sh
@@ -2,5 +2,7 @@
 
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.35.2/install.sh | bash
 
-nvm install 16.1.0
+echo 'You must now close and relaunch your terminal'
+echo 'Once that is complete run the command "nvm install 16.1.0"'
+
 
