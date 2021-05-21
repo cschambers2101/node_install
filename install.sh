8075c203354f062c2badcb948bfe8a7eb1078375
Craig Chambers <cchambers@salisbury6c.ac.uk>
2021-05-21 11:24:00 +0100

    updated auto load

--- install.sh
+++ install.sh
@@ -2,10 +2,9 @@
 
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.35.2/install.sh | bash
 
-echo 'You must now close and relaunch your terminal'
-
-source ~/.bashrc
-echo 'Once that is complete run the command "nvm install 16.1.0"'
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 nvm install 16.1.0
 
