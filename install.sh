ea58582a1588120122ddabdd9bc088201eab79eb
Craig Chambers <cchambers@salisbury6c.ac.uk>
2021-05-21 11:19:44 +0100

    Auto reload of terminal

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.35.2/install.sh | bash
 
 echo 'You must now close and relaunch your terminal'
+
+source ~/.bashrc
 echo 'Once that is complete run the command "nvm install 16.1.0"'
 
+nvm install 16.1.0
 
